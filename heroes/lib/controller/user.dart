ab112e0a6375b0029e36c6e1a3ec6690a900c6f5
gengjing-b <gengjing0513@163.com>
2020-06-27 01:24:02 +0800

    框架

--- heroes/lib/controller/user.dart
+++ heroes/lib/controller/user.dart
@@ -2,24 +2,52 @@ import 'package:aqueduct/aqueduct.dart';
 import 'package:heroes/heroes.dart';
 import 'package:heroes/model/hero.dart';
 
-class QuestionAddController extends ResourceController {
-  QuestionAddController(this.context);
+class UserController extends ResourceController {
+   UserController(this.context);
 
   final ManagedContext context;
-//store
- @Operation.post()
-Future<Response> createQuestion(@Bind.body(ignore: ["questionId"]) Question inputQuestion) async {
-  final query = Query<Question>(context)
-    ..values = inputQuestion;
 
-  final insertedQuestion = await query.insert();
+@Operation.get('username')
+Future<Response> getAllHeroes({@Bind.query('username') String username}) async {
+  final heroQuery = Query<Hero>(context);
+  if (username != null) {
+    heroQuery.where((user) => user.name).contains(username, caseSensitive: false);
+  }
+  final heroes = await heroQuery.fetch();
+     if (heroes != null)
+     { return Response.ok("ok");}
 
-  return Response.ok(insertedQuestion);
+   if (heroes == null) {
+      return Response.notFound();
+     }
 
-//check
-  @Operation.put('questionId')
-  Future<Response> checkQuestions() async {
-    return Response.ok("checkQuestions");
+
+  @Operation.get()
+  Future<Response> getAllHeroes() async {
+ 
+    final heroQuery = Query<Hero>(context);
+    final heroes = await heroQuery.fetch();
+   if (username == null) {
+      return Response.notFound();
+    return Response.ok(heroes);
+  }
+
+  @Operation.post('login')
+  Future<Response> getHeroByID(@Bind.path('username') String username) async {
+      print(username);
+    final heroQuery = Query<Hero>(context)..where((user) => user.name).equalTo(username);
+    final hero = await heroQuery.fetchOne();//fetchone从数据库中获取int类型的变量
+
+    if (hero == null) {
+      return Response.notFound();
+    }
+    
+    return Response.ok("");
+  }
+  @Operation.post('login')
+  Future<Response> login(@Bind.path('username') String username) async {
+      print(username);
+    return Response.ok("");
   }
 }
-}
+}}
